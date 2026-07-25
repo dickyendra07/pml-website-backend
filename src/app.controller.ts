f675786336b4aefddbe4a77f230ab3abc4c04ed2
Dicky Endra <dickyendra07@gmail.com>
2026-07-25 21:50:10 +0700

    Fix app controller api prefix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 
-@Controller('api')
+@Controller()
 export class AppController {
   constructor(
     private readonly prisma: PrismaService,
